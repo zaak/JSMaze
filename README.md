c38a21464df85694acd7081bab5d47be8bfe491d
Pawel Wiaderny <pawel.wiaderny@gmail.com>
2014-03-17 07:53:48 +0100

    Added timer. Done for now.

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-JSMaze: a simple maze engine based on [three.js](http://threejs.org/)
+## PigMaze: find the pig
+
+Remember the Maze game on Siemens phones?
 
 
-    Copyright(C) 2014 Pawel Wiaderny
+
+    Copyright (c) 2014 Pawel Wiaderny
     License: MIT License, see the file LICENSE.txt
 
 
 
-Sounds: http://www.freesfx.co.uk/sfx/pig
+Sounds: http://www.freesfx.co.uk/sfx/pig  
 Pig 3D model: http://tf3dm.com/3d-model/pig-13314.html
+
+
+
--- js/pigmaze/pigmaze.game.js
+++ js/pigmaze/pigmaze.game.js
@@ -1,11 +1,14 @@
 var PIGMAZE = {
     Game: function() {
         var _this = this;
-        var maze, canvasRenderer, webGLRenderer, finishCell, isGameOver = false, gameStarted = false;
-        
+        var maze, canvasRenderer, webGLRenderer, finishCell,
+                isGameOver = false, gameStarted = false,
+                randomSoundsInterval,
+                timeStarted, timerInterval, $timer;
+
         this.pigObject;
         this.pigObjectScale = 200;
-        
+
         var sounds = {
             random: [
                 new Audio('assets/sounds/pig_1.mp3'),
@@ -14,19 +17,17 @@ var PIGMAZE = {
             ],
             finish: new Audio('assets/sounds/pig_long.mp3')
         }
-        
-        var randomSoundsInterval;
 
         this.start = function() {
             maze = new MAZE.Maze();
             maze.generate();
-            
+
             // Get random finish cell on N wall
-            //finishCell = maze.getCell(_.random(0, 10), 0);
-            finishCell = maze.getCell(5, 9);
-            
+            finishCell = maze.getCell(_.random(0, 10), 0);
+            //finishCell = maze.getCell(5, 9);
+
             finishCell.isFinish = true;
-            
+
             console.log('FINISH', finishCell.x, finishCell.y);
 
             canvasRenderer = new MAZE.Renderer.Canvas({
@@ -37,9 +38,9 @@ var PIGMAZE = {
             webGLRenderer = new MAZE.Renderer.WebGL({
                 container: '#maze-container',
                 onCellChange: function(cell) {
-                    if(cell == null)
+                    if (cell == null)
                         return;
-                    
+
                     if (cell.x != canvasRenderer.currentCellX || cell.y != canvasRenderer.currentCellY)
                     {
                         canvasRenderer.currentCellX = cell.x;
@@ -51,15 +52,16 @@ var PIGMAZE = {
                         canvasRenderer.c2d.clearRect(0, 0, canvasRenderer.canvas.width(), canvasRenderer.canvas.height());
                         canvasRenderer.render();
                     }
-                    
+
                     _this.onCellChange(cell);
                 },
                 onFreeze: function(frozen) {
-                    if(!gameStarted) {
+                    if (!gameStarted) {
+                        gameStarted = true;
                         _this.gameStart();
                     }
-                    
-                    $('#maze-map-container-wrapper').toggle(frozen);   
+
+                    $('#maze-map-container-wrapper').toggle(frozen);
 
                     canvasRenderer.c2d.clearRect(0, 0, canvasRenderer.canvas.width(), canvasRenderer.canvas.height());
                     canvasRenderer.render();
@@ -77,67 +79,103 @@ var PIGMAZE = {
 //                pigObject.position.x = webGLRenderer.camera.position.x;
 //                pigObject.position.y = 0;
 //                pigObject.position.z = webGLRenderer.camera.position.z;
-                pigObject.rotation.y = - Math.PI / 2;
+                pigObject.rotation.y = -Math.PI / 2;
                 var s = _this.pigObjectScale;
                 pigObject.scale.set(s, s, s);
                 webGLRenderer.scene.add(pigObject);
                 _this.pigObject = pigObject;
 
             });
-            
+
             // Random pig sounds every 15 seconds
             randomSoundsInterval = setInterval(function() {
                 var sound = _.sample(sounds.random);
                 sound.play();
             }, 15000)
         }
-        
+
         this.onCellChange = function(cell) {
-            if(cell == finishCell)
+            if (cell == finishCell)
             {
                 _this.gameOver();
             }
         }
-        
+
         this.gameOver = function() {
-            if(isGameOver)
+            if (isGameOver)
                 return;
-            
+
             isGameOver = true;
-            
-            for(var i in sounds.random)
+
+            for (var i in sounds.random)
             {
                 clearInterval(randomSoundsInterval);
                 sounds.random[i].pause();
             }
-            
+
             sounds.finish.play();
-            
+
+            var rotationStep = 0.2;
             var rotatePigInterval = setInterval(function() {
-                _this.pigObject.rotation.y += 0.05;
+                _this.pigObject.rotation.y += rotationStep;
+                rotationStep += 0.01
             }, 10);
-            
+
             var shrinkPigInterval = setInterval(function() {
-                if(_this.pigObjectScale == 0)
+                if (_this.pigObjectScale == 0)
                 {
                     clearInterval(shrinkPigInterval);
                     clearInterval(rotatePigInterval);
                 }
-                
+
                 _this.pigObject.scale.set(_this.pigObjectScale, _this.pigObjectScale, _this.pigObjectScale);
                 _this.pigObjectScale--;
-            }, 100);
-            
+            }, 200);
+
+            clearInterval(timerInterval);
+            _this.flashText('Hooray! You found the pig!');
             console.log('Game Over!');
         }
-        
+
         this.gameStart = function() {
-            $('.pigmaze-hint').hide(); 
+            $('.pigmaze-hint').hide();
+            _this.flashText('Find the pig!');
+            _this.startTimer();
         }
-        
+
         this.setGodMode = function(enableGodmode)
         {
             webGLRenderer.setGodmode(enableGodmode);
+        }
+
+        this.flashText = function(text) {
+            $('<p id="flash-text">' + text + '</p>')
+                    .appendTo(document.body)
+                    .delay(3000)
+                    .fadeOut(1000, function() {
+                        $(this).remove();
+                    });
+        }
+
+        var timeEnd = Date.now();
+            
+        this.getTimeElapsed = function() {
+            var timeDiff = timeEnd - timeStarted;
+            var timeDiff = timeDiff / 1000;
+            var seconds = Math.round(timeDiff % 60);
+            timeDiff = Math.floor(timeDiff / 60);
+            var minutes = Math.round(timeDiff % 60);
+            return minutes + ':' + ("00" + seconds).slice(-2);
+        };
+
+        this.startTimer = function() {
+            timeStarted = Date.now();
+            $timer = $('<p id="pigmaze-timer">0:00</p>');
+            $timer.appendTo(document.body);
+            timerInterval = setInterval(function() {
+                timeEnd = Date.now();
+                $timer.text(_this.getTimeElapsed());
+            }, 1000)
         }
     }
 }
